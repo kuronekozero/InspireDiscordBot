a859a3de039f51ec619b0b21ab13912933a014bf
kuronekozero <materialisimo@mail.ru>
2021-09-13 22:51:52 +0300

    Initial commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Before using this Discord bot make sure that you created somewhere 
+in Python code "TOKEN" variable and write your Discord bot's token in it.
+
+Enjoy ^^
+
---
+++ inspire_bot.py
@@ -0,0 +1,27 @@
+import discord
+import requests
+import json
+
+client = discord.Client()
+
+def get_quote():
+  response = requests.get("http://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + "-" + json_data[0]['a']
+  return(quote)
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('!inspire_me'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+
+client.run(TOKEN)
